beeb500d978ca087ab8c7912b0abeb299556b9ff
c9s <cornelius.howl@gmail.com>
2012-03-18 02:01:12 +0800

    support many backends

--- src/SerializerKit/YamlSerializer.php
+++ src/SerializerKit/YamlSerializer.php
@@ -1,16 +1,56 @@
 <?php
 namespace SerializerKit;
+use sfYaml;
 
 class YamlSerializer
 {
+    const backend_yaml = 1;
+    const backend_syck = 2;
+    const backend_php = 3;
+
+    public $backend;
+
+    function __construct()
+    {
+        if( extension_loaded('syck') ) {
+            $this->backend = self::backend_syck;
+        }
+        elseif( extension_loaded('yaml') ) {
+            $this->backend = self::backend_yaml;
+        }
+        else {
+            $this->backend = self::backend_php;
+        }
+    }
+
     function encode($data) 
     {
-        return yaml_emit($data, YAML_UTF8_ENCODING ); 
+        switch($this->backend) {
+            case self::backend_syck:
+                return syck_dump( $data );
+                break;
+            case self::backend_yaml:
+                return yaml_emit($data, YAML_UTF8_ENCODING ); 
+                break;
+            case self::backend_php:
+                return sfYaml::dump($data,2);
+                break;
+        }
     }
 
     function decode($data) 
     {
-        return yaml_parse($data); 
+        switch($this->backend) {
+            case self::backend_syck:
+                return syck_load($data);
+                break;
+            case self::backend_yaml:
+                return yaml_parse($data); 
+                break;
+            case self::backend_php:
+                return sfYaml::load($data);
+                break;
+        }
     }
 }
 
